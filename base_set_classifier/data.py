b706a32b48f1270616b72317e87043890e5655d2
Evbor <23489366+Evbor@users.noreply.github.com>
2019-08-08 21:00:58 -0700

    added some functions to the data processing module

--- base_set_classifier/data.py
+++ base_set_classifier/data.py
@@ -1,1 +1,19 @@
-# test
+import pandas as pd
+import numpy as np
+import sklearn.model_selection as ms
+
+def train_val_test_split(df, test_proportion, val_proportion, random_seed):
+    filenames = df['filename'].values
+    labels = df['class'].values
+    filenames_train_val, filenames_test, labels_train_val, labels_test = ms.train_test_split(filenames, labels, test_size=test_proportion, stratify=labels, random_state=random_seed)
+    filenames_train, filenames_val, labels_train, labels_val = ms.train_test_split(filenames_train_val, labels_train_val, test_size=(val_proportion/(1 - test_proportion)), stratify=labels_train_val, random_state=random_seed)
+    return None
+
+
+def encode(label):
+    encoding = {'base_set': '1', 'not_base_set': '0'}
+    return encoding[label]
+
+def encode_dataframe(df):
+    df['class'] = df['class'].map(encode)
+    return df
