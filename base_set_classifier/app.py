b4fb71d241dee9110558388caf6a1bb48bcc706b
Evbor <23489366+Evbor@users.noreply.github.com>
2019-08-13 01:49:02 -0700

    redo

--- base_set_classifier/app.py
+++ base_set_classifier/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/dev/Desktop/'
@@ -28,7 +28,8 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file', filename=filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -38,6 +39,11 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 
 if __name__ == "__main__":
